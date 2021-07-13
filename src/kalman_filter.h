c65e0badf4985af02b3e679959c3d7e7ef792b84
Your Name <fizcris@gmail.com>
2021-07-13 22:05:29 +0200

    feat: added EKF support to kalmanFilter class

--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -21,11 +21,13 @@ class KalmanFilter {
    * @param P_in Initial state covariance
    * @param F_in Transition matrix
    * @param H_in Measurement matrix
+   * @param h_in measurement vector
    * @param R_in Measurement covariance matrix
    * @param Q_in Process covariance matrix
    */
   void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in,
-            Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_in, Eigen::MatrixXd &Q_in);
+            Eigen::MatrixXd &H_in, Eigen::VectorXd &h_in, Eigen::MatrixXd &R_in,
+            Eigen::MatrixXd &Q_in);
 
   /**
    * Prediction Predicts the state and the state covariance
@@ -60,6 +62,9 @@ class KalmanFilter {
 
   // measurement matrix
   Eigen::MatrixXd H_;
+
+  // measurement matrix
+  Eigen::VectorXd h_;
 
   // measurement covariance matrix
   Eigen::MatrixXd R_;
